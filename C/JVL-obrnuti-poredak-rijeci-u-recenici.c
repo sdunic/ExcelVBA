df2e14f5de36c397c66d4b66da9aa59c48b5bdec
Stefan Dunić <sdunic@gmail.com>
2019-02-20 15:53:13 +0100

    Jednostruko vezana lista, obrnuti poredak rijeci u recenici

---
+++ C/JVL-obrnuti-poredak-rijeci-u-recenici.c
@@ -0,0 +1,39 @@
+/*
+Ispis zadanog stringa s obrnutim poretkom rijeèi.
+*/
+#include <stdio.h>
+#include <string.h>
+
+typedef struct cvor{
+    char* tekst;
+    struct cvor *iduci;
+} rijec;
+
+int main()
+{
+    rijec *pocetak = NULL;
+
+	char str[] = "ja sam josip slipcevic";
+	int init_size = strlen(str);
+	char delim[] = " ";
+	char *ptr = strtok(str, delim);
+
+	while (ptr != NULL)
+	{
+	    rijec *nova = malloc(sizeof(rijec));
+
+	    nova->tekst = ptr;
+        nova->iduci = pocetak;
+        pocetak = nova;
+
+		ptr = strtok(NULL, delim);
+	}
+
+	while (pocetak != NULL)
+	{
+        printf("%s ", pocetak->tekst);
+        pocetak = pocetak->iduci;
+	}
+
+	return 0;
+}
